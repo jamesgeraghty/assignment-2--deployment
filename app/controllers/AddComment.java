607b3af2428ef87937a754b575f843a60d27684e
20022946 <20022946@mail.wit.ie>
2020-06-05 20:50:26 +0100

    add comment update

--- app/controllers/AddComment.java
+++ app/controllers/AddComment.java
@@ -20,5 +20,15 @@ public class AddComment extends Controller {
         render("dashboard.html", member, assessments);
 
     }
-
+    public static void addComment(String comment, Long id, Long assessmentid)
+    {
+        Logger.info("Adding a Comment");
+        Assessment assessment = Assessment.findById(id);
+        assessment.comment = comment;
+        assessment.save();
+        Trainer trainer = Accounts.getLoggedInTrainer();
+        Member member = Member.findById(id);
+        List<Assessment> assessments = member.assessments;
+        render("trainermemberdashboard.html", trainer, member, assessments);
+    }
 }
